7a0f96e2bbdea5b7e3b332f001c92959f67d1d3b
raiden <85284446+raiden-mod@users.noreply.github.com>
2024-01-06 21:34:50 +0100

    Update app.php

--- app.php
+++ app.php
@@ -1,25 +1,3 @@
-<?php
-// this will contain database root app root and file location
-// database params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'todo');
-
-$conn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
-$options = array(
-	PDO::ATTR_PERSISTENT => true,
-	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-);
-try {
-	$connection = new PDO($conn, DB_USER, DB_PASS, $options);
-	echo "connection successful";
-} catch (PDOException  $e) {
-	$error = $e->getMessage();
-	echo $error;
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
